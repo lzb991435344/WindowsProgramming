d3df683facad5708af7b71ab4b6a56a37be44a53
lzb991435344 <991435344@qq.com>
2020-02-04 20:42:54 +0800

    ErrorShow
    错误处理

--- Windows-via-C-Code/01-ErrorShow/ErrorShow.cpp
+++ Windows-via-C-Code/01-ErrorShow/ErrorShow.cpp
@@ -4,6 +4,35 @@ Notices: Copyright (c) 2008 Jeffrey Richter & Christophe Nasarre
 ******************************************************************************/
 
 
+/**
+1.数据类型
+   (1) VOID
+      改函数不可能失败
+   (2) BOOL
+      函数失败返回0,否则返回非0值
+   (3) HANDLE
+      函数失败返回NULL,否则将标识一个可以操纵的对象,有些函数返回-1(INVALID_HANDLE_VALUE)
+   (4) PVOID
+      函数失败返回NULL,否则标识一个数据块的内存地址
+   (5) LONG/DWORD
+      正常返回值是LONG/DWORD,查看sdk文档
+
+2.DWORD GetLastError()
+   当windows函数检测到错误时，会使用一种名为“线程本地存储区”的机制将相应的错误代码与主调线程关联到一起
+  这种机制使得不同的线程能够独立运行，不会出现相互干扰对方的错误代码。
+
+3.DWORD FormatMessage(
+ DWORD dwFlags,
+ LPCVOID pSource,
+ DWORD dwMwssageId,
+ DWORD dwLanguageId,
+ PTSTR pszBuffer,
+ DWORD nSize,
+ va_list* Arguments);
+
+ 将错误代码转换成相应的文本描述
+*/
+
 #include "..\CommonFiles\CmnHdr.h"     /* See Appendix A. */
 #include <Windowsx.h>
 #include <tchar.h>
@@ -55,6 +84,7 @@ void Dlg_OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify) {
 
    case IDOK:
       // Get the error code
+     //从编辑控件中获取错误代码
       DWORD dwError = GetDlgItemInt(hwnd, IDC_ERRORCODE, NULL, FALSE);
 
       HLOCAL hlocal = NULL;   // Buffer that gets the error message string
@@ -64,14 +94,18 @@ void Dlg_OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify) {
       DWORD systemLocale = MAKELANGID(LANG_NEUTRAL, SUBLANG_NEUTRAL);
 
       // Get the error code's textual description
+      //FORMAT_MESSAGE_FROM_SYSTEM 获得一个系统定义的错误代码对应的字符串
+      //FORMAT_MESSAGE_IGNORE_INSERTS 获得含有%号占位符的消息
+      //FORMAT_MESSAGE_ALLOCATE_BUFFER,  //分配内存，容纳错误文本内容的内存
       BOOL fOk = FormatMessage(
          FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS |
-         FORMAT_MESSAGE_ALLOCATE_BUFFER, 
+         FORMAT_MESSAGE_ALLOCATE_BUFFER, //分配内存，容纳错误文本内容的内存
          NULL, dwError, systemLocale, 
          (PTSTR) &hlocal, 0, NULL);
 
-      if (!fOk) {
+      if (!fOk) { //fok = 0
          // Is it a network-related error?
+         //从该dll中获取消息代码
          HMODULE hDll = LoadLibraryEx(TEXT("netmsg.dll"), NULL, 
             DONT_RESOLVE_DLL_REFERENCES);
 
@@ -85,6 +119,7 @@ void Dlg_OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify) {
          }
       }
 
+      //fok = 1
       if (fOk && (hlocal != NULL)) {
          SetDlgItemText(hwnd, IDC_ERRORTEXT, (PCTSTR) LocalLock(hlocal));
          LocalFree(hlocal);
